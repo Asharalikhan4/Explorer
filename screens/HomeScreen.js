f7698869d71ddffcad16b34cb39bdff631d316f0
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2022-12-25 16:39:32 +0530

    Head section complete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,29 @@
-import { View, Text } from "react-native"
-import React from "react";
+import { View, Text, SafeAreaView } from "react-native"
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, [])
   return (
-    <View>
-      <Text className="text-red-600">HomeScren</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1 relative">
+      <View className="flex-row px-6 mt-8 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
+        </View>
+        <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+      </View>
+
+      {/* SECOND SECTION */}
+      <View className="px-6 mt-8 space-y-3">
+        <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+        <Text>Good Moments</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
